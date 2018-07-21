f075429cba5183b9b5f4611fa60c8c7a8e187580
reinca <reinca97@naver.com>
2018-07-21 13:11:37 +0900

    Login btn style

--- src/utils/musescore.js
+++ src/utils/musescore.js
@@ -1,13 +1,10 @@
-import axios from 'axios'
 const parseString = require('xml2js').parseString;
 const http = require("http");
-
 //use MuseScore API
 
-const consumerKey = "857nCx8SQ5BVePHGiDKUyJnHjzBzAYKK";
-// ConsumerSecret: W2EG6GMEV9Nzt9jipGLkXr5c8vkzVLAu
 
 export const getMuseScoreData = (searchText, callback)=> {
+  const consumerKey = "857nCx8SQ5BVePHGiDKUyJnHjzBzAYKK";
 
   var url = `http://api.musescore.com/services/rest/score.xml?oauth_consumer_key=${consumerKey}&text=${searchText}&sort=relevance`;
 
--- src/utils/youtube.js
+++ src/utils/youtube.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-const YOUTUBE_API_KEY = 'AIzaSyA-70GHModCHjYNTcLa5i2-Gtql_8AjI6Y';
 let tokenUrl = "";
 
 export const getYoutubeData=function(url,callback){
+  const YOUTUBE_API_KEY = 'AIzaSyA-70GHModCHjYNTcLa5i2-Gtql_8AjI6Y';
   axios({
     method:"get",
     url:`https://www.googleapis.com/youtube/v3/search?maxResults=1&part=snippet&q=${url}&type=video&key=${YOUTUBE_API_KEY}${tokenUrl}`,
